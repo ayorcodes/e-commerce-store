f9b9ceb4ae03d432b2d7e3524d7af504778e0801
Sonia Uduma <sohnyauduma@gmail.com>
2021-07-15 21:15:07 +0100

    display products from firebase database

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AddProducts from "./components/AddProducts";
+import { ProductsContextProvider } from "./global/ProductsContext";
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <ProductsContextProvider>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/addproducts" component={AddProducts} />
           </Switch>
         </BrowserRouter>
-      </div>
+      </ProductsContextProvider>
     );
   }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductsContext } from "../global/ProductsContext";
+// import { CartContext } from "../Global/CartContext";
 
 const Products = () => {
-  return <div>Products</div>;
+  const { products } = useContext(ProductsContext);
+  console.log(products);
+
+  // const { dispatch } = useContext(CartContext);
+
+  return (
+    <>
+      {products.length !== 0 && <h1>Products</h1>}
+      <div className="products-container">
+        {products.length === 0 && (
+          <div>slow internet...no products to display</div>
+        )}
+        {products.map((product) => (
+          <div className="product-card" key={product.ProductID}>
+            <div className="product-img">
+              <img src={product.ProductImg} alt="not found" />
+            </div>
+            <div className="product-name">{product.ProductName}</div>
+            <div className="product-price">Rs {product.ProductPrice}.00</div>
+            <button className="addcart-btn">ADD TO CART</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Products;
